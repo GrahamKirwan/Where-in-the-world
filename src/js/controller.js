f0819c1ca4a340297f52cfb94974235610b722ee
graham.kirwan4@gmail.com <graham.kirwan4@gmail.com>
2021-04-10 14:27:39 -0400

    coutry info page functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,8 @@ const regions = document.querySelector('#regions');
 const theme = document.querySelector('#colour-theme');
 const countryOverlay = document.querySelector('.country_info');
 const backBtn = document.querySelector('.back_btn');
+const infoContainer = document.querySelector('#info-container');
+
 
 
 const apiCall = async function(country) {
@@ -29,6 +31,27 @@ const findRegions = async function(region) {
         capital = data[i].capital;
         generateHtml(flag, population, region, name, capital);
     }
+}
+
+const countryInfo = async function(country) {
+    let res = await fetch(`https://restcountries.eu/rest/v2/name/${country}`);
+    let data = await res.json();
+
+        let name, flag, population, region, capital, nativeName, subRegion, topLevelDomain, currencies, languages, borders;
+        name = data[0].name;
+        name = name.charAt(0).toUpperCase() + name.slice(1);
+        nativeName = data[0].nativeName;
+        subRegion = data[0].subregion;
+        flag = data[0].flag;
+        population = data[0].population;
+        region = data[0].region;
+        capital = data[0].capital;
+        topLevelDomain = data[0].topLevelDomain[0];
+        currencies = data[0].currencies[0].name;
+        languages = data[0].languages;
+        borders = data[0].borders;
+        generateInfoHtml(flag, population, region, name, capital, currencies, topLevelDomain, nativeName, subRegion, languages, borders)
+    
 }
 
 
@@ -84,15 +107,53 @@ resultsContainer.addEventListener('click', function(e){
         let country = e.path[2].getAttribute('name');
         renderCountryDetails(country);
     }
+    
 })
 
 function renderCountryDetails(country) {
     countryOverlay.style.display = 'initial';
     resultsContainer.style.display = 'none';
+    countryInfo(country);
+    
 }
 
 backBtn.addEventListener('click', function(){
     countryOverlay.style.display = 'none';
     resultsContainer.style.display = 'flex';
-
+    document.querySelector('#info_container_inner').remove();
 })
+
+function generateInfoHtml(flag, population, region, name, capital, currencies, topLevelDomain, nativeName, subRegion, languages, borders) {
+
+    
+    let html =  `
+        <div class="info_container" id="info_container_inner">
+            <div class="info_container_left">
+                <img src="${flag}" alt="${name}">
+            </div>
+            <div class="info_container_right">
+                <h2 class="info_container_right--heading">${name}</h2>
+                <div class="info_container_right--info">
+                    <div class="info_container_right--info--left">
+                        <p><strong>Native Name: </strong>${nativeName}</p>
+                        <p><strong>Population: </strong>${population}</p>
+                        <p><strong>Region: </strong>${region}</p>
+                        <p><strong>Sub Region: </strong>${subRegion}</p>
+                        <p><strong>Capital: </strong>${capital}</p>
+                    </div>
+                    <div class="info_container_right--info--right">
+                        <p><strong>Top Level Domain: </strong>${topLevelDomain}</p>
+                        <p><strong>Currencies: </strong>${currencies}</p>
+                        <p><strong>Languages: </strong>${languages}</p>
+                    </div>
+                </div>
+                <div class="info_container_right--borders">
+                    <p><strong>Border Countries: </strong><span>France</span><span>Germany</span><span>Netherlands</span></p>
+                </div>
+            </div>
+        </div>
+    `
+    
+    infoContainer.insertAdjacentHTML('beforeend', html);
+
+}
